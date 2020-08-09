8022ec0f8fe2098362f2ad3c978d06d21a253a5f
Elze Hamilton <elze.hamilton@gmail.com>
2020-08-09 10:46:44 -0500

    Converted function App into component App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export class App extends Component {
+  render() {	
   return (
     <div className="App">
       <header className="App-header">
@@ -22,6 +23,7 @@ function App() {
       </header>
     </div>
   );
+ }
 }
 
 export default App;
