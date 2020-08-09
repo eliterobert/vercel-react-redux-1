3dd1e24dbaa704288b97e4f4105368a3b0d71321
Elze Hamilton <elze.hamilton@gmail.com>
2020-08-09 13:57:08 -0500

    Displaying date retrieved from the API

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 export class App extends Component {
-  static defaultProps = { currentDate: ""};
+  //static defaultProps = { currentDate: ""};
+  
+  constructor(props) {
+    super(props);
+    this.state = {currentDate: (new Date('2020-08-08')).toString()};
+  }  
 	
   getDate = async () => {
     const response = await fetch('/api/date');
@@ -16,13 +21,23 @@ export class App extends Component {
     return body;
   };	
 	
+  getItems = async () => {
+    const response = await fetch('/api/items');
+	console.log(`getDate: returned from fetch`);
+    const body = await response.json();
+	console.log(`getDate: got response.json() ; response.status = ${response.status}`);
+    if (response.status !== 200) {
+		throw Error(body.message);
+	}
+    return body;
+  };	
+  
   componentDidMount() {
     this.getDate()
 	  .then(res => {
 		console.log(`componentDidMount: res = ${res}`);
-		//this.props.currentDate = res.toString();
-		this.props.currentDate = res();
-		console.log(`componentDidMount: this.props.currentDate = ${this.props.currentDate}`);
+		this.setState({ currentDate: res })
+		//console.log(`componentDidMount: this.props.currentDate = ${this.props.currentDate}`);
 	  })
       .catch(err => console.log(err));
   }  
@@ -31,19 +46,10 @@ export class App extends Component {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Current date is:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-			<p>{ this.props.currentDate } </p>		
+			<p>{ this.state.currentDate } </p>	
       </header>
     </div>
   );
