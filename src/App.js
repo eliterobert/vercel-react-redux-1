ca23ca977096a4ad64c4bb4790420f0ac7c35b88
Elze Hamilton <elze.hamilton@gmail.com>
2020-08-09 10:30:33 -0500

    Providing a callback for fetch promise

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-		  <p>{ fetch('/api/date').toString() } </p>
+		  <p>{ fetch('/api/date').then(response => response.toString()) } </p>
         </a>
       </header>
     </div>
